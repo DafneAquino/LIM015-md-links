99c171375200df8ba3e4253aa613769f995c8a7e
DafneAquino <dafneaquinom@gmail.com>
2021-10-06 13:16:59 -0500

    termino de las funciones del CLI

--- src/cli.js
+++ src/cli.js
@@ -1,21 +1,19 @@
 #!/usr/bin/env node
 
-const mdLinks = require('./mdlinks');
+const { mdLinks } = require('./mdlinks');
 const option = require('./options');
 const colors = require('colors');
 
-// Para que a futuro la matriz de argumentos inicie en el índice 2
+// Para que a futuro la matriz de argumentos inicie en el índice 2 como posición
 const arguments = process.argv.slice(2);
-// console.log(arguments.length);
-// console.log(`Hola mundo ${arguments}`);
 
 // Si el usuario pone un argumento
 if (arguments.length === 1){
     mdLinks(arguments[0], { validate:false })
     .then(resul=>resul.forEach(e=> console.log(
-        `${colors.blue(e.href)}
-        ${colors.yellow(e.text)}
-        ${colors.pink(e.file)}`
+        `link : ${colors.blue(e.href)}
+text: ${colors.yellow(e.text)}
+file: ${colors.cyan(e.file)}`
      )))
     .catch(err => console.log(err));
 }
@@ -26,12 +24,12 @@ if(arguments.length === 2){
     switch (arguments[1]) {
         case '--validate':
             mdLinks(arguments[0], { validate: true })
-            .then(res => res.forEach(el => console.log(
-                `${colors.blue(el.href)} 
-                 ${colors.yellow(el.text)} 
-                 ${colors.pink(el.file)} 
-                 ${colors.green(el.status)} 
-                 ${colors.yellow(el.message)}`
+            .then(res => res.forEach(el =>
+                console.log(`link : ${colors.blue(el.href)} 
+text: ${colors.yellow(el.text)} 
+file: ${colors.cyan(el.file)} 
+status: ${colors.green(el.status)} 
+message: ${colors.yellow(el.message)}`
              )))
             .catch(err => console.log(err));
         break;
@@ -40,7 +38,7 @@ if(arguments.length === 2){
             mdLinks(arguments[0], { validate: true })
             .then(res=> console.log(
                 `${colors.blue(option.totalLinks(res))} 
-                ${colors.yellow(option.uniqueLinks(res))}`
+${colors.yellow(option.uniqueLinks(res))}`
              ))
             .catch(err => console.log(err));
         break;
@@ -56,19 +54,17 @@ if(arguments.length === 2){
 };
 
 // Si el usuario pone 3 argumentos
-
 if(arguments.length === 3){
     if ( arguments[1]=== '--stats' && arguments[2] === '--validate'){
         mdLinks(arguments[0], { validate: true })
-            .then(res=> console.log(`
-            ${colors.blue(option.totalLinks(res))} 
-            ${colors.yellow(option.uniqueLinks(res))} 
-            ${colors.pink(option.brokenLinks(res))}`
+            .then(res=> console.log(
+`Total: ${colors.blue(option.totalLinks(res))} 
+Unique: ${colors.yellow(option.uniqueLinks(res))} 
+Broken: ${colors.cyan(option.brokenLinks(res))}`
             ))
             .catch(err => console.log(err));
-        break;
     }
     else{
-        console.log('Sorry, the command does not exist. Try with command "--help"')
+        console.log(colors.brightRed('Sorry, the command does not exist. Try with command "--help"'))
     }
 }
--- src/options.js
+++ src/options.js
@@ -1,31 +1,33 @@
+const colors = require('colors');
+
 /* Función para obtener la cantidad total de los links(guardados como objetos en un array) encontrados en la ruta*/
-const totalLinks = (arrayLinks) => `Total: ${arrayLinks.lenght}`;
+const totalLinks = (arrayLinks) => `${arrayLinks.lenght}`;
 
 // Función para obtener la cantidad de los links unicos(unique) encontrados en la ruta
 const uniqueLinks = (arrayLinks) => {
     const linksSet = new Set ([]);// almacena valores únicos irrepetibles
     arrayLinks.forEach((element) => linksSet.add(element.href));
-    return `Unique: ${newSet.size}`
+    return `${linksSet.size}`
 };
 
 // Función para obtener la cantidad de los links rotos(broken) encontrados en la ruta
 const brokenLinks = (arrayLinks) => {
     const broken = arrayLinks.filter((e)=> e.status >= 400 );
-    return `Broken: ${broken.lenght}`
+    return `${broken.lenght}`
 };
 
 const help = `
 ╔══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗
-║                                                       **HELP**                                                       ║
+║                                                      ${colors.green('**HELP** ')}                                                       ║
 ╠══════════════════════════════════════╦═══════════════════════════════════════════════════════════════════════════════╣
-║ ${color.green('--validate')}         ║ Show the link, its text, its route, its status number and its status message. ║
+║ ${colors.green('--validate')}                           ║ Show the link, its text, its route, its status number and its status message. ║
 ╠══════════════════════════════════════╬═══════════════════════════════════════════════════════════════════════════════╣
-║ ${color.green('--stats')}            ║ Show the total links and the unique links.                                    ║
+║ ${colors.green('--stats')}                              ║ Show the total links and the unique links.                                    ║
 ╠══════════════════════════════════════╬═══════════════════════════════════════════════════════════════════════════════╣
-║ ${color.green('--stats --validate')} ║ Show the total, unique and broken links.                                      ║
+║ ${colors.green('--stats --validate')}                   ║ Show the total, unique and broken links.                                      ║
 ╚══════════════════════════════════════╩═══════════════════════════════════════════════════════════════════════════════╝
 ${colors.yellow(`Use this structure : mdLinks <route> <command> to run the cli. 
-But also you can write only mdLinks <route> and you will obtain the link, its text, its file`)}` 
+But also you can write only mdLinks <route> and you will obtain the link, its text and its file.`)}` 
 
 module.exports = {
     totalLinks,
